5631ce90c6eebfd9defafeffe5cbf9275e585c6b
antonhansel <antoninribeaud@gmail.com>
2016-01-30 13:53:14 -0800

    Basic sun with skybox

---
+++ StarSystem/Assets/rotate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		// Cachhe the transform link
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,20*Time.deltaTime,0);
+	}
+}
